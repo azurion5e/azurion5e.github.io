3feb2bfd3650f7c679e8b1628b374a4758147013
Valery Romanov <ikrakozabra@gmail.com>
2020-03-21 00:30:57 +0300

    + Woodoo magic page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     siteTitleShort: `gatsby-theme-docs`,
     siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
     siteUrl: `https://rocketdocs.netlify.com`,
-    siteAuthor: `@rocketseat`,
+    siteAuthor: `i-madness`,
     siteImage: `/banner.png`,
     siteLanguage: `en`,
     themeColor: `#7159c1`,
